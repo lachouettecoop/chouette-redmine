c90c4eda6d3a3794f4e7d947df697573d9ad6582
tyndare <tyndare@wanadoo.fr>
2017-04-10 20:24:41 +0200

    ckeditor: force top not collapsed toolbar pour le Wiki

---
+++ data/redmine/plugins/redmine_ckeditor/lib/redmine_ckeditor/wiki_formatting/helper.rb
@@ -0,0 +1,66 @@
+module RedmineCkeditor::WikiFormatting
+  module Helper
+    def replace_editor_tag(field_id)
+      javascript_tag <<-EOT
+      $(document).ready(function() {
+        #{replace_editor_script(field_id)}
+      });
+      EOT
+    end
+
+    def replace_editor_script(field_id)
+      <<-EOT
+      (function() {
+        var id = '#{field_id}';
+        var textarea = document.getElementById(id);
+        if (!textarea) return;
+
+        var editor_options = #{RedmineCkeditor.options(@project).to_json};
+        if (window.location.href.indexOf("/wiki/") > 0) {
+            editor_options.toolbarCanCollapse = false;
+            editor_options.toolbarLocation = 'top';
+        }
+        var editor = CKEDITOR.replace(textarea, editor_options);
+        editor.on("change", function() { textarea.value = editor.getSnapshot(); });
+      })();
+      EOT
+    end
+
+    def overwrite_functions
+      javascript_tag <<-EOT
+        function showAndScrollTo(id, focus) {
+          var elem = $("#" + id);
+          elem.show();
+          if (focus != null && CKEDITOR.instances.hasOwnProperty(focus)) { CKEDITOR.instances[focus].focus(); }
+          $('html, body').animate({scrollTop: elem.offset().top}, 100);
+        }
+
+        function destroyEditor(id) {
+          if (CKEDITOR.instances[id]) CKEDITOR.instances[id].destroy();
+        }
+      EOT
+    end
+
+    def initial_setup
+      overwrite_functions
+    end
+
+    def wikitoolbar_for(field_id)
+      if params[:format] == "js"
+        javascript_tag(replace_editor_script(field_id))
+      else
+        ckeditor_javascripts +
+        stylesheet_link_tag('editor', :plugin => 'redmine_ckeditor') +
+        initial_setup + replace_editor_tag(field_id)
+      end
+    end
+
+    def initial_page_content(page)
+      "<h1>#{ERB::Util.html_escape page.pretty_title}</h1>"
+    end
+
+    def heads_for_wiki_formatter
+    end
+  end
+end
+
