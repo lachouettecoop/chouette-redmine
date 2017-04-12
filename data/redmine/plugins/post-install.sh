1938b2e0d60b09c72e10889d623bc3c3799c7d95
tyndare <tyndare@wanadoo.fr>
2017-04-12 13:05:41 +0200

    ckeditor: modif droit d'accès répertoire rich

--- data/redmine/plugins/post-install.sh
+++ data/redmine/plugins/post-install.sh
@@ -3,8 +3,13 @@
 # Intall plugins' ruby dependencies (Gemfile):
 (cd /usr/src/redmine/plugins &&
 for d in */; do
-    cd "$d" && test -f Gemfile && bundle install;
+    cd "$d" && test -f Gemfile && (bundle install || exit -1);
 done)
+
+# Modification de droits d'accès spécialement pour 
+# le plugin CKEditor, ce répertoire étant mappé 
+# depuis le répertoire data:
+chown redmine /usr/src/redmine/public/system/rich
 
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake redmine:plugins:migrate
